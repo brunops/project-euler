6406dd5136de4ac46a929bc89221caf4f278dafb
Bruno Sanches <brunopsanches@gmail.com>
2014-01-29 02:16:56 -0800

    Problem 7 - Improve performance by checking if a number is prime in a novel way, execution time improved from 4.5s to 0.5s lololol

--- problem-7/10001st-prime.py
+++ problem-7/10001st-prime.py
@@ -3,27 +3,28 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
 # What is the 10 001st prime number?
+from math import sqrt
+
+def is_prime(number):
+  for x in range(2, int(sqrt(number)) + 1):
+    if number % x == 0:
+      return False
+
+  return True
+
 def prime(position):
-  primes = [2]
   current_position = 1
   current_number = 3
 
   # calculate primes until reaching desired position
   while current_position < position:
-    is_prime = True
-    for prime in primes:
-      if current_number % prime == 0:
-        is_prime = False
-        break
-
-    if is_prime:
-      primes.append(current_number)
+    if is_prime(current_number):
       current_position += 1
 
     # try only odd numbers (started with 3)
     current_number += 2
 
-  return primes[len(primes) - 1]
+  return current_number
 
 
 print prime(10001)
