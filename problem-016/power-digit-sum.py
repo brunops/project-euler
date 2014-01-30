5d3e50b86098aef4fc9a4e57558bff61e6b012fc
Bruno Sanches <brunopsanches@gmail.com>
2014-01-30 14:44:39 -0800

    Problem 16 - Power digit sum .. i like how python handles calculations :)

---
+++ problem-016/power-digit-sum.py
@@ -0,0 +1,9 @@
+# Power digit sum
+# Problem 16
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#
+# What is the sum of the digits of the number 2^1000?
+def power_digit_sum(num, power):
+  return reduce((lambda x, y: x + y), map((lambda x: int(x)), list(str(num**power))))
+
+print power_digit_sum(2, 1000)
