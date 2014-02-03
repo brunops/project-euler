925e0a7bd9dfcb848955fad5b22158c0944e989d
Bruno Sanches <brunopsanches@gmail.com>
2014-02-02 17:43:59 -0800

    Problem 21 - Sum of all amicable numbers until 10000

--- problem-021/amicable-numbers.py
+++ problem-021/amicable-numbers.py
@@ -1,7 +1,7 @@
 # Amicable numbers
 # Problem 21
 # Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
-# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+# If d(a) = b and d(b) = a, where a != b, then a and b are an amicable pair and each of a and b are called amicable numbers.
 # 
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 # 
@@ -13,5 +13,27 @@ def divisors(n):
       divs.append(i)
 
   return divs
-    
   
+def sum_of_divisors(until):
+  divisors_sum = {}
+  for i in range(1, until + 1):
+    divisors_sum[i] = sum(divisors(i))
+
+  return divisors_sum
+
+  
+def amicable_numbers(until):
+  divisors_sum = sum_of_divisors(until)
+  amicables = []
+  for i in range(1, until + 1):
+    if divisors_sum[i] in divisors_sum \
+      and i != divisors_sum[i] \
+      and i == divisors_sum[divisors_sum[i]]:
+        amicables.append(i)
+
+  return amicables
+
+def amicable_numbers_sum(until):
+  return sum(amicable_numbers(until))
+
+print amicable_numbers_sum(10000)
