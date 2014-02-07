9d03d197ae2c56bb34cfa44b2feb0126b9dbd4bb
Bruno Sanches <brunopsanches@gmail.com>
2014-02-06 21:50:04 -0800

    remove commented spiral matrix

--- problem-028/spiral-diagonals.py
+++ problem-028/spiral-diagonals.py
@@ -11,18 +11,6 @@
 # It can be verified that the sum of the numbers on the diagonals is 101.
 #
 # What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
-
-#  111 112 113 114 115 116 117 118 119 120 121
-#  110 73  74  75  76  77  78  79  80  81  82
-#  109 72  43  44  45  46  47  48  49  50  83
-#  108 71  42  21  22  23  24  25  26  51  84
-#  107 70  41  20  7   8   9   10  27  52  85
-#  106 69  40  19  6   1   2   11  28  53  86
-#  105 68  39  18  5   4   3   12  29  54  87
-#  104 67  38  17  16  15  14  13  30  55  88
-#  103 66  37  36  35  34  33  32  31  56  89
-#  102 65  64  63  62  61  60  59  58  57  90
-#  101 100 99  98  97  96  95  94  93  92  91
 def print_matrix(matrix):
   for i in range(0, len(matrix)):
     print matrix[i]
