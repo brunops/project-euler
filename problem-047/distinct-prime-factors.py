38a5abf1c5854c675e002041f26cf21aa9f6dc10
Bruno Sanches <brunopsanches@gmail.com>
2014-02-07 20:37:24 -0800

    Problem 47 - Print 4 consecutive numbers with 4 distinct prime factors
    * Define distinct_prime_factors function which is responsible to elevate numbers to it's power
    * Simplify distinct_arrays moving logic to distinct_prime_factors
    * Implement distinct_prime_factors_consecutive_numbers logic

--- problem-047/distinct-prime-factors.py
+++ problem-047/distinct-prime-factors.py
@@ -13,6 +13,8 @@
 #
 # Find the first four consecutive integers to have four distinct prime factors. What is the first of these numbers?
 
+import copy
+
 # Generate primes array using the Sieve of Eratosthenes
 def primes_until(until):
   primes = [2]
@@ -42,9 +44,9 @@ def primes_until(until):
   }
 
 
-
 # use global to store primes list - blargh :P
-primes_list = primes_until(5000)
+primes_list = primes_until(5000000)
+print 'sieve generated'
 def next_prime(n):
   prime_index = primes_list['primes_index'][n]
   return primes_list['primes'][prime_index + 1]
@@ -63,6 +65,20 @@ def prime_factors(n):
 
   return factors
 
+def distinct_prime_factors(n):
+  factors = prime_factors(n)
+
+  # map factors to a dictionary with the count of each element
+  factors_count = dict([(x, factors.count(x)) for x in set(factors)])
+
+  # map arrays to its corresponding power
+  # [2, 2, 3] => [4, 3]
+  distinct_factors = []
+  for number in factors_count.keys():
+    distinct_factors.append(number**factors_count[number])
+
+  return distinct_factors
+
 def distinct_prime_factors_consecutive_numbers(total_factors):
   # first number
   numbers = [2]
@@ -74,30 +90,34 @@ def distinct_prime_factors_consecutive_numbers(total_factors):
   number = 2
 
   while len(numbers) < total_factors:
-
+    factors = distinct_prime_factors(number)
+    # possible valid result
+    if len(factors) == total_factors:
+      numbers_factors_copy = copy.deepcopy(numbers_factors)
+      numbers_factors_copy.append(factors)
+      # add number to result
+      if distinct_arrays(numbers_factors_copy):
+        numbers_factors.append(factors)
+        numbers.append(number)
+      # start over
+      else:
+        numbers_factors = [factors]
+        numbers = [number]
+    # start over, numbers must be consecutive
+    else:
+      numbers_factors = []
+      numbers = []
 
     number += 1
 
   return numbers
 
 def distinct_arrays(arrays):
-  # map arrays to a dictionary with a count of each element
-  arrays = map((lambda array: dict([(x, array.count(x)) for x in set(array)])), arrays)
+  all_numbers = []
+  for array in arrays:
+    all_numbers += array
 
-  # store all factors
-  all_factors = []
-  # map arrays to its corresponding power
-  # [[2, 2], [2]] => [[4], [2]]
-  for i in range(len(arrays)):
-    factors = []
-    for number in arrays[i].keys():
-      factors.append(number**arrays[i][number])
-
-    all_factors += factors
-
-  return len(all_factors) == len(set(all_factors))
+  return len(all_numbers) == len(set(all_numbers))
 
 
-print distinct_arrays([[2,2], [2, 2]])
-# print distinct_prime_factors_consecutive_numbers(2)
-# print distinct_prime_factors_consecutive_numbers(3)
+print distinct_prime_factors_consecutive_numbers(4)
