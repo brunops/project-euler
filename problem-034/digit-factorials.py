5c44ee5e2158022b5fa56f14f0b10845809b6e28
Bruno Sanches <brunopsanches@gmail.com>
2014-03-04 16:51:26 -0800

    Problem 34 - Digit factorial sum

--- problem-034/digit-factorials.py
+++ problem-034/digit-factorials.py
@@ -8,12 +8,32 @@
 
 factorials = {}
 def factorial(number):
-  if number <= 2:
-    return number
+  if number <= 1:
+    return 1
   elif number in factorials:
     return factorials[number]
   else:
     factorials[number] = number * factorial(number - 1)
     return factorials[number]
 
+def digit_factorial_sum(number):
+  return sum(map(lambda x: factorial(int(x)), list(str(number))))
 
+def is_digit_factorial(number):
+  return number == digit_factorial_sum(number)
+
+def total_digit_factorials_sum():
+  # upper bound is 9! * 7 = 2540160, since 9! * 8 results in a 7 digit number
+  upper_bound = 2540161
+  # exclude numbers that are not sums
+  total = 0
+  curr = 10
+  while curr < upper_bound:
+    if is_digit_factorial(curr):
+      total += curr
+
+    curr += 1
+
+  return total
+
+print total_digit_factorials_sum()
