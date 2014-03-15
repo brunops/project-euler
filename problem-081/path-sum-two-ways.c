845293ca9cdfc0e12a16508f7ae9c457b1bdbfe2
Bruno Sanches <brunopsanches@gmail.com>
2014-03-14 23:34:40 -0700

    Populate matrix with all numbers from matrix.txt file

--- problem-081/path-sum-two-ways.c
+++ problem-081/path-sum-two-ways.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
+#define MATRIX_SIZE 80
 
 int main() {
-  char curr;
+  int curr_number, row, col, i, j;
+  int matrix[MATRIX_SIZE][MATRIX_SIZE];
 
-  char buffer[BUFFER_SIZE];
+  char buffer[BUFFER_SIZE], *token;
   FILE *fp;
+
 
   if ((fp = fopen("matrix.txt", "r")) == NULL) {
     printf("Failed opening file\n");
@@ -15,10 +19,18 @@ int main() {
 
 
   while (fgets(buffer, sizeof(char) * BUFFER_SIZE, fp)) {
-    printf("%s", buffer);
-  }
+    token = strtok(buffer, ",");
+    matrix[row][0] = atoi(token);
 
+    col = 0;
+    while ((token = strtok(NULL, ","))) {
+      matrix[row][col++] = atoi(token);
+    }
+
+    row++;
+  }
   fclose(fp);
+
 
   return 0;
 }
