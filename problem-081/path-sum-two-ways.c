4ff8e4456b4a00d854321641cd9074fb9ca660c0
Bruno Sanches <brunopsanches@gmail.com>
2014-03-14 23:51:59 -0700

    Problem 81 - Path Sum: two ways in C .... 0.005s ._.

--- problem-081/path-sum-two-ways.c
+++ problem-081/path-sum-two-ways.c
@@ -5,6 +5,8 @@
 #define BUFFER_SIZE 1024
 #define MATRIX_SIZE 80
 
+#define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
+
 int main() {
   int curr_number, row, col, i, j;
   int matrix[MATRIX_SIZE][MATRIX_SIZE];
@@ -18,11 +20,12 @@ int main() {
   }
 
 
+  row = 0;
   while (fgets(buffer, sizeof(char) * BUFFER_SIZE, fp)) {
     token = strtok(buffer, ",");
     matrix[row][0] = atoi(token);
 
-    col = 0;
+    col = 1;
     while ((token = strtok(NULL, ","))) {
       matrix[row][col++] = atoi(token);
     }
@@ -31,6 +34,21 @@ int main() {
   }
   fclose(fp);
 
+  // Calculate paths for last row and last column
+  for (i = MATRIX_SIZE - 2; i >= 0; i--) {
+    matrix[MATRIX_SIZE - 1][i] += matrix[MATRIX_SIZE - 1][i + 1];
+    matrix[i][MATRIX_SIZE - 1] += matrix[i + 1][MATRIX_SIZE - 1];
+  }
+
+
+
+  for (i = MATRIX_SIZE - 2; i >= 0; i--) {
+    for (j = MATRIX_SIZE - 2; j >= 0; j--) {
+      matrix[i][j] += MIN(matrix[i + 1][j], matrix[i][j + 1]);
+    }
+  }
+
+  printf("Smallest sum path: %d\n", matrix[0][0]);
 
   return 0;
 }
